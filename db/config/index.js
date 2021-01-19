b83731280ea58cbaa1ac00fc35cc2ee8d1e33334
Tiago Mendes <tiago.mendes@dsbsolucoes.com.br>
2021-01-19 16:04:25 -0300

    Fixing Bugs

--- db/config/index.js
+++ db/config/index.js
@@ -1,19 +1,7 @@
 module.exports = {
-    HOST: "0.0.0.0",
-    USER: "root",
-    PASSWORD: "",
-    DB: "db_exam_lab",
+    HOST: process.env.HOST || "0.0.0.0",
+    USER: process.env.USER || "root",
+    PASSWORD: process.env.PASS || "",
+    DB: process.env.DB || "db_exam_lab",
     PORT: 3306,
-
-
-    // HOST: "sql10.freemysqlhosting.net",
-    // USER: "sql10387865",
-    // PASSWORD: "prlUhtnRQJ",
-    // DB: "db_exam_lab"
 };
-
-// Server: sql10.freemysqlhosting.net
-// Name: sql10387865
-// Username: sql10387865
-// Password: prlUhtnRQJ
-// Port number: 3306
