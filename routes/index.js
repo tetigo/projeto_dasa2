9f8826fd99b47d6effaf37b93621d99905ceda68
Tiago Mendes <tiago.mendes@dsbsolucoes.com.br>
2021-01-19 16:07:53 -0300

    Adding Root Route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const labsRouter = require('./labs')
 const init = () => {
     const router = require('express').Router()
 
+    router.use('/', (req, res) => {
+        res.status(200).send({ msg: "OK! Agora para ver os exames e laboratorios digite: https://dasa-project.herokuapp.com/exams ou https://dasa-project.herokuapp.com/labs" })
+    })
+
     router.use('/exams', examsRouter())
     router.use('/labs', labsRouter())
 
